f58be6faa7e3feb927177d61a69ab7881a59ff93
galDeri23 <Galderi1999@gmail.com>
2025-02-02 16:59:04 +0200

    Fix the save in Firestore

--- app/src/main/java/com/example/finalproject_breathe_again/SignUpActivity.kt
+++ app/src/main/java/com/example/finalproject_breathe_again/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.example.finalproject_breathe_again
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -18,23 +19,26 @@ class SignUpActivity : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
 
         binding.singUp.setOnClickListener {
             val name = binding.singUpName.text.toString()
             val email = binding.singUpEmail.text.toString()
             val password = binding.singUpPassword.text.toString()
+            val cigarettes = binding.singUpCigarettes.text.toString().toInt()
 
-            if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+            if (name.isEmpty() || email.isEmpty() || password.isEmpty() || cigarettes == 0) {
                 Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show()
             } else if (password.length < 6) {
                 Toast.makeText(this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show()
             } else {
-                registerUser(name, email, password)
+                registerUser(name, email, password , cigarettes)
             }
         }
     }
 
-    private fun registerUser(name: String, email: String, password: String) {
+    private fun registerUser(name: String, email: String, password: String, cigarettes: Int) {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
@@ -42,13 +46,16 @@ class SignUpActivity : AppCompatActivity() {
                     val user = mapOf(
                         "name" to name,
                         "email" to email,
+                        "cigarettes" to cigarettes,
                         "userId" to userId
                     )
                     firestore.collection("users").document(userId!!)
                         .set(user)
                         .addOnSuccessListener {
                             Toast.makeText(this, "User registered successfully!", Toast.LENGTH_SHORT).show()
-                            finish()
+                            val intent = Intent(this, MainActivity::class.java)
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            startActivity(intent)
                         }
                         .addOnFailureListener { e ->
                             Toast.makeText(this, "Error saving user: ${e.message}", Toast.LENGTH_LONG).show()
